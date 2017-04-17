a28644b005fab55d1262497189c4db2af4335281
kisakyegordon <gordon56692@gmail.com>
2017-04-17 21:51:41 +0300

    My 1st commit for slc

--- hello.py
+++ hello.py
@@ -1,33 +1,15 @@
-#def make_breakfast():
-#	print("Welcome\n These are your options for breakfast")
-#	print("1. Eggs \n 2. Pancakes \n")
-#	cook = input("What food do you want")
-#	if cook == "eggs":
-#		print("So you want eggs")
-
-#	print("Breakfast")
-#	print("1. Get the ingredients")
-#	print("2. Beat the Eggs")
-#	print("3. Mix any desired ingtreidients with the eggs")
-#	print("4. Heat up your source of heat and heat up some cooking oil")
-#	print("5. Put the ready eggs in the oil, cook, turn eggs")
-#	print("6. Ready !!")
-#	breakfast= "------Yummy -------" + cook
-#	return breakfast
-
-#print(make_breakfast("Egg"))
-#print(make_breakfast("fISH"))
-#make_breakfast();
-
 def make_cook():
 	print("Get the Ingredients")
 	print("Mix")
 	print("Pour Into Heated pan")
 	print("Turn to other side")
+	return
 
-def make_breakfast1(food, served_with):
+def make_breakfast(food):
 	make_cook()
-	if len(served_with) == 0:
-		print("------ Yummy-----" + food )
+#	if len(served_with) == 0:
+	print("------ Yummy-----" + food )
 
-make_breakfast1(eggs, sauce)
+
+
+make_breakfast("apple")
