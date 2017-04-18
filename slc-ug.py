90fda66b4ec098dec3f01a2858084d2c9c331052
kisakyegordon <gordon56692@gmail.com>
2017-04-18 05:21:17 +0300

    Contains function without testing

--- slc-ug.py
+++ slc-ug.py
@@ -1,3 +1,16 @@
 
 print("\nSLC-UG Day 1 Task\n")
 
+
+def print_prime():
+	n = 20
+	#print(int(input))
+	for p in range(2, n+1):
+		for i in range(2, p):
+			if p % i == 0:
+				break
+		else:
+			print p,
+	print('\nDone')
+	
+print_prime()
